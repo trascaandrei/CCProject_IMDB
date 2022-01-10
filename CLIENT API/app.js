df91c6d4694a58f8096fe508c467f97b3dd492f3
Andrei Trasca <trascaandrei17@gmail.com>
2022-01-10 23:30:05 +0200

    project setup

---
+++ CLIENT API/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+/* Connect to Mongoose */
+mongoose.connect(process.env.DB_PATH, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('connected to db successfully');
+}).catch((err) => {
+    console.error('Failed to connect to db: ', err);
+});
+
+// Use Api routes in the App
+// require("./routes/api-routes")(app);
+
+app.listen(port, function() {
+    console.log(`Running on port ${port}"..."`);
+});
+
